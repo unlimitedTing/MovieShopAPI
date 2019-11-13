66d579063e8032603794a175937d26dadfbd6788
unlimitedTing <liu673@wustl.edu>
2019-11-13 15:40:01 -0500

    add unit test project

---
+++ MovieShop.UnitTest/MovieServiceTest.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovieShop.UnitTest
+{
+    [TestClass]
+    public class MovieServiceTest
+    {
+        [TestMethod]
+        public void TestMovieFromFakeDta1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        public void TestMovieFromFakeDta2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        public void TestMovieFromFakeDta3()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        public void TestMovieFromFakeDta4()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        public void TestMovieFromFakeDta5()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
